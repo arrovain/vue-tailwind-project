5adfcab8796b659ba0f01b824fb74ea2e26068da
Mahmut İÇME <arrovain@gmail.com>
2024-07-03 11:33:44 +0300

    onSubmit function

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-
+import { useRouter } from 'vue-router';
+import { ref, onMounted } from 'vue';
 import { useCategoryStore } from '@/stores/categoryStore';
+const { displayLoader, destroyLoader } = useGlobalLoader();
+
 
 const store = useCategoryStore();
+const onSubmit = async () => {
+  try {
+    displayLoader();
+
+    await store.createCategory(form.value);
+    router.push('/');
+  } catch (error) {
+    console.log(error);
+  } finally {
+    destroyLoader();
+  }
+};
 
 </script>
 
