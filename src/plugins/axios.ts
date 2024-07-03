ffecadf32bdd80e86b101aecdb59e47002ca85a2
Mahmut İÇME <arrovain@gmail.com>
2024-07-03 10:55:53 +0300

    authStore

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -3,3 +3,5 @@ import axios from 'axios'
 const axiosInstance = axios.create({
     baseURL: 'https://api.freeapi.app/api/v1'
 })
+
+export default axiosInstance
---
+++ src/stores/authStore.ts
@@ -0,0 +1,11 @@
+
+import { defineStore } from 'pinia'
+import axios from '@/plugins/axios'
+
+export const useAuthStore = defineStore('AuthStore', {
+    state: () => ({
+        user: getLocalStorageData<User | null>('currentUserContent', true)
+    }),
+
+    actions: {},
+})
